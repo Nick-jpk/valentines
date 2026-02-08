f6e8d6a4ec3a52b46b0d82abbfe650e742ce2f30
Nick-jpk <nickheavens52@gmail.com>
2026-02-08 06:14:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+const message = document.getElementById("message");
+
+btn.addEventListener("click", () => {
+  message.textContent = "You are loved more than you know 💖✨";
+});
